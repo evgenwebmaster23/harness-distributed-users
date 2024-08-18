3afbacf3bd9c13e8fda6100869ae77352bd5401e
evgenwebmaster23 <161200325+evgenwebmaster23@users.noreply.github.com>
2024-08-18 05:32:13 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,14 @@
+    main()
+
+if __name__ == "__main__":
+    for item in data:
+def generate_random_data():
+
+        print(f"Random Number: {item}")
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+    data = generate_random_data()
+import random
+def main():
